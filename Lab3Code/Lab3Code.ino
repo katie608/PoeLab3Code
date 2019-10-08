eecd90ef641a76454fe2be90ef1332e788de2d5e
Jerry Goss <jerry.h.goss@gmail.com>
2019-10-08 12:06:20 -0400

    Fixed serialprint and enabled motor functionality

--- Lab3Code/Lab3Code.ino
+++ Lab3Code/Lab3Code.ino
@@ -1,5 +1,15 @@
 // Include the Servo library 
 #include <Servo.h> 
+
+// Include the required Servo Libraries
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+
 Servo Servo1;
 Servo Servo2;
 void setup() {
@@ -7,15 +17,26 @@ void setup() {
   Serial.println("Setup");
   Servo1.attach(11);
   Servo2.attach(9);
+  AFMS.begin();
+  myMotor1->setSpeed(150);
+  myMotor2->setSpeed(150);
 }
 
 void loop() {
 //  Serial.println("A0:");
-  Serial.println(analogRead(A0));
+  long sensor1 = analogRead(A0);
+  long sensor2 = analogRead(A1);
+
+  myMotor1->run(FORWARD);
+  myMotor2->run(FORWARD);
+  
+  Serial.print(sensor1);
+  Serial.print("\t");
+  Serial.println(sensor2);
 //  Serial.println("A1:");
 //  Serial.println(analogRead(A1));
-  Servo1.write(i);
-  Servo2.write(j);
+//  Servo1.write(i);
+//  Servo2.write(j);
   
 
 }
