d49e00c9562084baabe04491e5e9f641687cb8cc
Katherine Foster <kfoster@olin.edu>
2019-10-11 12:23:39 -0400

    Add more comments

--- Lab3Code/Lab3Code.ino
+++ Lab3Code/Lab3Code.ino
@@ -12,31 +12,34 @@ Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
 
 Servo Servo1;
 Servo Servo2;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Setup");
+
+  // set up motors
   Servo1.attach(11);
   Servo2.attach(9);
+
+  // begin adafruit motor shield
   AFMS.begin();
-  myMotor1->setSpeed(150);
-  myMotor2->setSpeed(150);
+  myMotor1->setSpeed(50);
+  myMotor2->setSpeed(50);
 }
 
 void loop() {
-//  Serial.println("A0:");
-  long sensor1 = analogRead(A0);
-  long sensor2 = analogRead(A1);
-
+  // set sensors equal to variables so they can be used in calculaiton
+  long sensor1 = analogRead(A0); // on the left
+  long sensor2 = analogRead(A1); // on the right
+  
+  // proportional control: (difference / range) * constant
+  
   myMotor1->run(FORWARD);
   myMotor2->run(FORWARD);
-  
+
+  // display IR sensor values
+  // IR values are about 952 when pointed at floor, and 1002 when pointed at tape
   Serial.print(sensor1);
   Serial.print("\t");
   Serial.println(sensor2);
-//  Serial.println("A1:");
-//  Serial.println(analogRead(A1));
-//  Servo1.write(i);
-//  Servo2.write(j);
-  
-
 }
