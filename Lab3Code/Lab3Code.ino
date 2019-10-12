a695d1cb68e17480fdbc5c0820bcf7222750f74f
Jerry Goss <jerry.h.goss@gmail.com>
2019-10-12 12:30:24 -0400

    Updating Serial Outputs for CSV Compatibility

--- Lab3Code/Lab3Code.ino
+++ Lab3Code/Lab3Code.ino
@@ -1,7 +1,4 @@
-// Include the Servo library 
-#include <Servo.h> 
-
-// Include the required Servo Libraries
+ // Include the required Servo Libraries
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
@@ -25,7 +22,6 @@ void setup() {
 
 void loop() {
   // set sensors equal to variables so they can be used in calculaiton
-
   if (Serial.available() > 0 ) {
     char temp = Serial.read();
     if (temp == '+') {
@@ -41,14 +37,23 @@ void loop() {
   myMotor1->run(BACKWARD);
   myMotor2->run(BACKWARD);
   
-  myMotor1->setSpeed(10*(sensor1-sensor2)/50+speed_constant);
-  myMotor2->setSpeed(10*(sensor2-sensor1)/50+speed_constant);
+  int m1_speed = (14*(sensor1-sensor2)/50+speed_constant);
+  int m2_speed = (14*(sensor2-sensor1)/50+speed_constant);
+  
+  myMotor1->setSpeed(m1_speed);
+  myMotor2->setSpeed(m2_speed);
   
   // display IR sensor values
   // IR values are about 952 when pointed at floor, and 1002 when pointed at tape
   Serial.print(sensor1);
-  Serial.print("\t");
-  Serial.println(sensor2);
-  Serial.print("\t");
-  Serial.print(speed_constant);
+  Serial.print(",");
+  Serial.print(sensor2);
+  Serial.print(",");
+//  Serial.print(speed_constant);
+//  Serial.print(",");
+  Serial.print(m1_speed);
+  Serial.print(",");
+  Serial.println(m2_speed);
+  
+  
 }
