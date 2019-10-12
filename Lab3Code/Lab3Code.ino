1e870e292475c662284d5c31bc1d1d9751b26d4c
Katherine Foster <kfoster@olin.edu>
2019-10-12 13:09:11 -0400

    Final changes (printing all data, comments)

--- Lab3Code/Lab3Code.ino
+++ Lab3Code/Lab3Code.ino
@@ -11,6 +11,7 @@ Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
 
 
 void setup() {
+  // initialize serial
   Serial.begin(9600);
   Serial.println("Setup");
 
@@ -21,7 +22,7 @@ void setup() {
 }
 
 void loop() {
-  // set sensors equal to variables so they can be used in calculaiton
+  // use serial port to increment speed
   if (Serial.available() > 0 ) {
     char temp = Serial.read();
     if (temp == '+') {
@@ -31,26 +32,31 @@ void loop() {
       speed_constant = speed_constant - 10;
     }
   }
+
+  // set sensors equal to variables so they can be used in calculaiton
   long sensor1 = analogRead(A0); // on the left
   long sensor2 = analogRead(A1); // on the right
 
+  // set the direction of the motors in the direction we want the robot to move
   myMotor1->run(BACKWARD);
   myMotor2->run(BACKWARD);
-  
+
+  // calculate the speed of each motor using proportional control
   int m1_speed = (14*(sensor1-sensor2)/50+speed_constant);
   int m2_speed = (14*(sensor2-sensor1)/50+speed_constant);
-  
+
+  // set speed of each motor
   myMotor1->setSpeed(m1_speed);
   myMotor2->setSpeed(m2_speed);
   
   // display IR sensor values
   // IR values are about 952 when pointed at floor, and 1002 when pointed at tape
+  Serial.print(millis());
+  Serial.print(",");
   Serial.print(sensor1);
   Serial.print(",");
   Serial.print(sensor2);
   Serial.print(",");
-//  Serial.print(speed_constant);
-//  Serial.print(",");
   Serial.print(m1_speed);
   Serial.print(",");
   Serial.println(m2_speed);
